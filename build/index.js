a1406cc0cf11c68e215ef4f502bb33506ae9842b
Jessica Gardner <jssgrdnr@gmail.com>
2021-04-02 13:47:16 +0000

    feature progression, ascendant and moon sign

--- build/index.js
+++ build/index.js
@@ -1,16 +1,100 @@
 #!/usr/bin/env node
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 exports.__esModule = true;
 var prompt = require("prompt-promise");
 var horoscope_1 = require("horoscope");
-var promiseOfADate = prompt("What is your birthday? Please enter MM.DD.YYYY ");
-promiseOfADate.then(function (query) {
-    var queryMonthAsString = query.split(".")[0];
-    var queryDayAsString = query.split(".")[1];
-    var queryMonthAsNumber = Number(queryMonthAsString);
-    var queryDayAsNumber = Number(queryDayAsString);
-    console.log("Your sun sign is *.*.*.", horoscope_1.getSign({ month: queryMonthAsNumber, day: queryDayAsNumber }), ".*.*.*");
-}).then(function () {
-    prompt.finish();
-});
+var circular_natal_horoscope_js_1 = require("circular-natal-horoscope-js");
+// const promiseOfALocation: Promise<string> = prompt("Location? ");
+var main = function () {
+    return __awaiter(this, void 0, void 0, function () {
+        var date, MonthAsString, DayAsString, YearAsString, MonthAsNumber, DayAsNumber, YearAsNumber, time, HourAsString, MinuteAsString, HourAsNumber, MinuteAsNumber, location, LatAsString, LongAsString, LatAsNumber, LongAsNumber, origin, horoscope;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, prompt("What is your birthday? Please enter MM.DD.YYYY ")];
+                case 1:
+                    date = _a.sent();
+                    MonthAsString = date.split(".")[0];
+                    DayAsString = date.split(".")[1];
+                    YearAsString = date.split(".")[2];
+                    MonthAsNumber = Number(MonthAsString);
+                    DayAsNumber = Number(DayAsString);
+                    YearAsNumber = Number(YearAsString);
+                    console.log("Your sun sign is: *.*.*.", horoscope_1.getSign({ month: MonthAsNumber, day: DayAsNumber }), ".*.*.*");
+                    return [4 /*yield*/, prompt("What is your time of birth? Please enter in HH:MM ")];
+                case 2:
+                    time = _a.sent();
+                    HourAsString = time.split(":")[0];
+                    MinuteAsString = time.split(":")[1];
+                    HourAsNumber = Number(HourAsString);
+                    MinuteAsNumber = Number(MinuteAsString);
+                    return [4 /*yield*/, prompt("And the location? Please enter the city in Latitude, Longitude to 2 decimal points: ")];
+                case 3:
+                    location = _a.sent();
+                    LatAsString = location.split(",")[0];
+                    LongAsString = location.split(",")[1];
+                    LatAsNumber = Number(LatAsString);
+                    LongAsNumber = Number(LongAsString);
+                    origin = new circular_natal_horoscope_js_1.Origin({
+                        year: YearAsNumber,
+                        month: MonthAsNumber - 1,
+                        date: DayAsNumber,
+                        hour: HourAsNumber,
+                        minute: MinuteAsNumber,
+                        latitude: LatAsNumber,
+                        longitude: LongAsNumber
+                    });
+                    horoscope = new circular_natal_horoscope_js_1.Horoscope({
+                        origin: origin,
+                        houseSystem: "whole-sign",
+                        zodiac: "tropical",
+                        aspectPoints: ['bodies', 'points', 'angles'],
+                        aspectWithPoints: ['bodies', 'points', 'angles'],
+                        aspectTypes: ["major", "minor"],
+                        customOrbs: {},
+                        language: 'en'
+                    });
+                    console.log("Your ascendant is: ", horoscope.Ascendant.Sign.label, "and your moon sign is: ", horoscope.CelestialBodies.moon.Sign.label);
+                    console.log("Have a pleasant day");
+                    return [2 /*return*/];
+            }
+        });
+    });
+};
+main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,61 @@
 #!/usr/bin/env node
 
-import * as prompt from "prompt-promise"
-import {getSign} from "horoscope"
+import * as prompt from "prompt-promise";
+import { getSign } from "horoscope";
+import { Origin, Horoscope } from "circular-natal-horoscope-js";
+import { cityMap } from "city-lat-long-map"
 
-const promiseOfADate: Promise<string> = prompt("What is your birthday? Please enter MM.DD.YYYY ")
+// const promiseOfALocation: Promise<string> = prompt("Location? ");
 
-promiseOfADate.then(function (query: string) {
-  const queryMonthAsString: string = query.split(".")[0]
-  const queryDayAsString: string = query.split(".")[1]
-  const queryMonthAsNumber: number = Number(queryMonthAsString)
-  const queryDayAsNumber: number = Number(queryDayAsString)
+const main = async function () {
+  const date: string = await prompt("What is your birthday? Please enter MM.DD.YYYY ");
 
-  console.log("Your sun sign is *.*.*.", getSign({month: queryMonthAsNumber, day: queryDayAsNumber}), ".*.*.*")
+  const MonthAsString: string = date.split(".")[0]
+  const DayAsString: string = date.split(".")[1]
+  const YearAsString: string = date.split(".")[2]
+  const MonthAsNumber: number = Number(MonthAsString)
+  const DayAsNumber: number = Number(DayAsString)
+  const YearAsNumber: number = Number(YearAsString)
 
-}).then(function () {
-  prompt.finish();
-  })
+  console.log("Your sun sign is: *.*.*.", getSign({ month: MonthAsNumber, day: DayAsNumber }), ".*.*.*");
+
+  const time: string = await prompt("What is your time of birth? Please enter in HH:MM ");
+
+  const HourAsString: string = time.split(":")[0]
+  const MinuteAsString: string = time.split(":")[1]
+  const HourAsNumber: number = Number(HourAsString)
+  const MinuteAsNumber: number = Number(MinuteAsString)
+
+  const location: string = await prompt("And the location? Please enter the city in Latitude, Longitude to 2 decimal points: ");
+
+  const LatAsString: string = location.split(",")[0]
+  const LongAsString: string = location.split(",")[1]
+  const LatAsNumber: number = Number(LatAsString)
+  const LongAsNumber: number = Number(LongAsString)
+
+  const origin = new Origin({
+    year: YearAsNumber,
+    month: MonthAsNumber - 1,
+    date: DayAsNumber,
+    hour: HourAsNumber,
+    minute: MinuteAsNumber,
+    latitude: LatAsNumber,
+    longitude: LongAsNumber,
+  });
+
+  const horoscope = new Horoscope({
+    origin,
+    houseSystem: "whole-sign",
+    zodiac: "tropical",
+    aspectPoints: ['bodies', 'points', 'angles'],
+    aspectWithPoints: ['bodies', 'points', 'angles'],
+    aspectTypes: ["major", "minor"],
+    customOrbs: {},
+    language: 'en'
+  });
+
+  console.log("Your ascendant is: ", horoscope.Ascendant.Sign.label, "and your moon sign is: ", horoscope.CelestialBodies.moon.Sign.label)
+  console.log("Have a pleasant day")
+}
+
+main()
